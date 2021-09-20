8edaeb12be2b5b71408acbc1abc875c378e9a738
GreenGrassBlueOcean <43173895+GreenGrassBlueOcean@users.noreply.github.com>
2021-09-20 08:08:24 +0200

    Update test-CheckforSlaveClusters.R

--- tests/testthat/test-CheckforSlaveClusters.R
+++ tests/testthat/test-CheckforSlaveClusters.R
@@ -23,7 +23,83 @@ library(RClusterWatcher)
     print(MasterClusterHash)
 
     SlaveClusterHashes <- foreach::foreach(cl = cl, .errorhandling = 'pass', .packages = c("data.table", "archivist", "RClusterWatcher"),.export = c("AddNewClustertoDB", "GetStartedClusterPIDData", "GetProcessData")) %dopar% {
-      requireNamespace("RClusterWatcher", quietly = TRUE)
+
+      #0. load functions that can not be available in github workflows
+      AddNewClustertoDB <- function( cl = NULL
+                                     , clusterName = "super cluster"
+                                     , MasterCluster = NA
+                                     , DBdir = .Options$RClusterWatcher.DBdir
+                                     , verbose = FALSE
+      ){
+        if(is.null(cl)){
+          stop("cluster object must be supplied")
+        }
+
+        ClusterData <- GetStartedClusterPIDData(cl)
+        if (verbose){
+          print(ClusterData)
+        }
+
+        ClusterData$ProcesState <- NULL
+        attr(ClusterData, "tags" ) = c(paste0("clusterName:",clusterName), paste0("MasterCluster:",MasterCluster))
+        hash <- archivist::saveToLocalRepo(repoDir = DBdir
+                                           , artifact = ClusterData
+                                           # , userTags =  #MasterCluster
+                                           , use_flocks = T
+        )
+
+        message(paste0(capture.output(ClusterData), collapse = "\n"))
+
+        return(hash)
+      }
+
+      GetStartedClusterPIDData <- function(cluster){
+        Host <- GetProcessData(PID = Sys.getpid())
+        Host$Role <- "Host"
+        Nodes <- parallel::clusterCall(cl = cluster, fun = GetProcessData) %>%
+          data.table::rbindlist()
+
+        Nodes$Role <- "worker"
+        RETURNtable <- data.table::rbindlist(list(Host,Nodes))
+        return(RETURNtable)
+      }
+
+      GetProcessData <- function(PID = Sys.getpid()){
+
+        if(!identical(Sys.info()['sysname'][[1]], "Windows")){
+          stop("only implemented for Windows")
+        }
+
+        ExecPS <- function(x){system2("powershell", args = x , stdout = T)}
+
+        ConvertPSTimeDate <- function(PSCommand){
+          as.POSIXct(PSCommand,format="%m/%d/%Y %H:%M:%S %p",tz=Sys.timezone())
+        }
+
+
+        GetProcessDataCommand <- paste0("tasklist /FI 'pid eq ",PID,"' /FO CSV")
+        GetProcessStartTime <- paste0("Get-Process -ID ",PID," | select starttime")
+        GetLastBootTimeCommand <- "Get-CimInstance -ClassName win32_operatingsystem | select lastbootuptime"
+
+
+        PSProcessOutput <- ExecPS(GetProcessDataCommand)
+        LastBootTime <- ConvertPSTimeDate(ExecPS(GetLastBootTimeCommand)[4])
+
+        if(identical(PSProcessOutput, "INFO: No tasks are running which match the specified criteria.")){
+          return(data.table::data.table( PID = PID
+                                         , ProcesState = "down"
+                                         , SystemBootTime = LastBootTime))
+        } else {
+          ProcessData <- data.table::fread(text = PSProcessOutput)
+          ProcessData$ProcessStartTime <- ConvertPSTimeDate(ExecPS(GetProcessStartTime)[4])
+          ProcessData$SystemBootTime = LastBootTime
+          ProcessData$ProcesState = "Running"
+          ProcessData
+        }
+      }
+
+      ## End of loading functions
+
       rl <- parallel::makePSOCKcluster(names = 2)
       doParallel::registerDoParallel(rl)
       # MainCluster <-  GetStartedClusterPIDData(rl)
